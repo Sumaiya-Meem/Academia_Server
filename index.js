e3ec00b453fb1be3d2c0affd1f8a88d58feded7d
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-04-04 12:28:43 +0600

    put user in database

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
         res.send(result);
     })
 
+    
+   app.get('/users/:email',async(req, res) => {
+    const email = req.params.email;
+    const query = {email: email};
+    const result = await userCollection.findOne(query);
+    res.send(result); 
+})
+
 
 
 
